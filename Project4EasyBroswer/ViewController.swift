50e57544bc84fc4e6a5cf67b7a7ba7af05ced8ea
henry22 <henry811010@gmail.com>
2015-05-05 17:33:49 +0800

    Although some errors,when adding these codes two things happen: 1.You must conform to the protocol. This is a fancy way of saying, "if you're telling me you can handle being my delegate, here are the methods you need to implement." In the case of navigationDelegate, all these methods are optional, meaning that we don't need to implement any methods. 2.Any methods you do implement will now be given control over the WKWebView's behavior. Any you don't implement will use the default behavior of WKWebView.

--- Project4EasyBroswer/ViewController.swift
+++ Project4EasyBroswer/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Override the actual loading of the view – we don't want that empty thing on the storyboard, we want our own code
+    override func loadView() {
+        //Create a new instance of Apple's WKWebView web browser component and assign it to a variable called webView
+        webView = WKWebView()
+        //Setting the web view's navigationDelegate property to self, which means "when any web page navigation happens,please tell me."
+        webView.navigationDelegate = self
+        //Make our view (the root view of the view controller) that web view
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
