f6a3a4173dfabe5900c2a950c45025e7407288b7
henry22 <henry811010@gmail.com>
2015-05-05 16:48:52 +0800

    Initial Commit

---
+++ Project4EasyBroswer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project4EasyBroswer
+//
+//  Created by Henry on 5/5/15.
+//  Copyright (c) 2015 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
